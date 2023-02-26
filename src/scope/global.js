3d0f9e440cf7334eb2448f8d6e5c79b2fd6d3c1a
jackie2806 <jaishackeline@gmail.com>
2023-02-25 23:06:11 -0500

    Ordenando ideas

--- src/scope/global.js
+++ src/scope/global.js
@@ -1,14 +1,15 @@
-//Declaración de variables
-var a;
-//Asignación de variables
-var a = 'a';
-//Normalmente la declaración y la asignación se hacen en una sola línea
-var b = 'b';
-//reasignación de variables
-var b = 'bb';
+var a; //Declaración de variables
 
-//variable golabes
-var fruit = 'Apple';
+var a = 'a'; //Asignación de variables
+
+var b = 'b'; //Normalmente la declaración y la asignación se hacen en una sola línea
+
+b = 'bb'; //Reasignación de variables
+
+var a = 'aa'; //redeclaración de variables
+console.log(a)
+//Global Scope
+var fruit = 'Apple'; //global
 
 console.log(fruit)
 
