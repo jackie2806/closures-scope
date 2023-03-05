4cdd3d75cb2fb6449d1c2d9df2bc946bbb097688
jackie2806 <jaishackeline@gmail.com>
2023-03-05 17:08:32 -0500

    use strict

---
+++ strict.js
@@ -0,0 +1,4 @@
+// cuando se usa 'use strict' no hoisting (elevación y asignación de las variables )
+'use strict'
+pi = 3.1416
+console.log(pi) // no se ejecuta esta línea cuando se usa 'use strict'
