78f424629fbb6500780a9a3eddb261820a799cf1
jackie2806 <jaishackeline@gmail.com>
2023-03-07 08:44:04 -0500

    block scope de var

--- src/scope/block.js
+++ src/scope/block.js
@@ -1,13 +1,14 @@
 function fruits(){
     //variables definidas dentro de un bloque solo pueden ser accedidas dentro del scopce de bloque
     if(true){
-         var fruit1 = 'Apple'; //function scop
+         var fruit1 = 'Apple'; //function scope
          let fruit2 = 'Kiwi'; //block scope
          const fruit3 = 'Banana'; //block scope
+         console.log(fruit1) // var también tiene block scope, además de function scope
     }
     console.log(fruit1);
-    console.log(fruit2); //No se puede acceder
-    console.log(fruit3) // No se puede acceder
+    //console.log(fruit2); //No se puede acceder
+    //console.log(fruit3) // No se puede acceder
         
     
 }
